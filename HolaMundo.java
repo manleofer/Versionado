0a4035d56b4e30dfd97355a69cc3e923249f0811
manleofer <mleofer304@iesalmudeyne.es>
2024-10-15 10:17:15 +0200

    Tasteando el etiquetado de versiones

---
+++ HolaMundo.java
@@ -0,0 +1,8 @@
+/**
+ * Muestra por pantalla la frase "¡Hola mundo!"
+ */
+public class HolaMundo{
+    public static void main (String[] args){
+        System.out.println("¡Hola mundo!");
+    }
+}
